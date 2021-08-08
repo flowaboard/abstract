1257d0318e4a1002f016df8bd83dd5ebfc4225aa
Manoj Choudhury <aj.manoj99@gmail.com>
2021-08-08 09:54:04 +0000

    abstract entities

---
+++ index.js
@@ -0,0 +1,21 @@
+import {DesignElement,FlowDesigns} from 'https://flowaboard.github.io/flowaboard/front-end/flowdesign/design.js';
+
+var abstractDesign = new FlowDesigns.ListDesign('Abstract', 'abstract', `https://en.wikipedia.org/wiki/Abstraction`);
+abstractDesign.add(new DesignElement('Mathematics', 'mathematics', `https://en.wikipedia.org/wiki/Mathematics`, 'flow-info', 'https://flowaboard.github.io/flowaboard/mathematics/index.js'));
+abstractDesign.add(new DesignElement('Programming', 'programming', `https://en.wikipedia.org/wiki/Computer_programming`,'flow-info','https://flowaboard.github.io/flowaboard/programming/index.js'));
+abstractDesign.add(new DesignElement('AI', 'ai', 'https://en.wikipedia.org/wiki/Artificial_intelligence','flow-info','https://flowaboard.github.io/flowaboard/artificial_intelligence/index.js'));
+abstractDesign.add(new DesignElement('Business', 'business', 'https://en.wikipedia.org/wiki/Business','flow-info','https://flowaboard.github.io/flowaboard/business/index.js'));
+
+abstractDesign.flowConfig = {
+    flex: true,
+    defaultValue: {
+        widthfactor: abstractDesign.designElements.length,
+        xPadding: 0.4,
+        yPadding: 0.4,
+    },
+    elementAction: {
+        "click" : {"action":"flow","state":"default"}
+    }
+}
+
+export default abstractDesign;
