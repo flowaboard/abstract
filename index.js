a7ecb6406ff3e0842b86249ba9661f61b41a3e46
Manoj Choudhury <aj.manoj99@gmail.com>
2022-06-22 01:04:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import {DesignElement,FlowDesigns} from 'https://flowaboard.github.io/flowaboard
 var abstractDesign = new FlowDesigns.ListDesign('Abstract', 'abstract', `https://en.wikipedia.org/wiki/Abstraction`);
 abstractDesign.add(new DesignElement('Mathematics', 'mathematics', `https://en.wikipedia.org/wiki/Mathematics`, 'flow-info', 'https://flowaboard.github.io/flowaboard/front-end/flowdesign/mathematics/index.js'));
 abstractDesign.add(new DesignElement('Programming', 'programming', `https://en.wikipedia.org/wiki/Computer_programming`,'flow-info','https://flowaboard.github.io/flowaboard/front-end/flowdesign/programming/index.js'));
-abstractDesign.add(new DesignElement('AI', 'ai', 'https://en.wikipedia.org/wiki/Artificial_intelligence','flow-info','https://flowaboard.github.io/flowaboard/front-end/flowdesign/artificial_intelligence/index.js'));
+abstractDesign.add(new DesignElement('AI', 'ai', 'https://en.wikipedia.org/wiki/Artificial_intelligence','flow-info','https://flowaboard.github.io/flowaboard/front-end/flowdesign/ai/index.js'));
 abstractDesign.add(new DesignElement('Business', 'business', 'https://en.wikipedia.org/wiki/Business','flow-info','https://flowaboard.github.io/flowaboard/front-end/flowdesign/business/index.js'));
 
 abstractDesign.flowConfig = {
